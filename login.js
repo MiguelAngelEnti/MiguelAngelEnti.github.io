c6cd1bc8f6ef7458292d9770561c866d3b29c631
MiguelAngelEnti <miguelangel.latre@enti.cat>
2025-01-17 11:28:22 +0100

    more

---
+++ login.js
@@ -0,0 +1,28 @@
+function process()
+{
+	event.preventDefault();
+	
+	console.log("Procesando");
+	
+	let nombre = document.getElementById("form_user");
+	
+	let salida = document.getElementById("exit");
+	
+	if (nombre.value.length < 2)
+	{
+		salida.value = "El nobre debe tener al menos 2 caracteres.";
+		
+		nombre.style.color = "#ff0000";
+		
+		nombre.style.border = "1px solid black";
+		
+		salida.style.color = "#ff0000";
+		
+		nombre.focus();
+		
+		return false;
+	}
+	
+	document.getElementById("form").submit();
+	
+}
